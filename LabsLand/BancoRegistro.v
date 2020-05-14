efab874555dff9c0ea05e4d2e3711fb44e0e5411
Juan David Sandoval Suárez <62728815+jdsandoval9@users.noreply.github.com>
2020-05-14 16:14:07 -0500

    LabsLand

---
+++ LabsLand/BancoRegistro.v
@@ -0,0 +1,81 @@
+
+module BancoRegistro /*#(      		 //   #( Parametros
+         parameter BIT_ADDR = 8,  //   BIT_ADDR Número de bit para la dirección
+         parameter BIT_DATO = 4,  //  BIT_DATO  Número de bit para el dato
+			parameter   RegFILE= "src/Reg16.men")
+	(
+    input [BIT_ADDR-1:0] addrRa,
+    input [BIT_ADDR-1:0] addrRb,
+    
+	 output [BIT_DATO-1:0] datOutRa,
+    output [BIT_DATO-1:0] datOutRb,
+    
+	 input [BIT_ADDR:0] addrW,
+    input [BIT_DATO-1:0] datW,
+    */
+	 //input RegWrite,
+    output [6:0]G_HEX0,
+    output [6:0]G_HEX1,
+    input [9:0] V_SW,
+    //input clk,
+    input G_CLOCK_50,
+    input [1:0] V_BT);
+    
+
+// Aignacion de reloj
+wire clk;
+assign clk = G_CLOCK_50;
+
+//asignacion de reset
+
+wire rst;
+assign rst = V_BT[0];
+
+//asignación Escribir
+
+wire RegWrite;
+assign RegWrite = V_BT[1];
+
+//Número de bit para la dirección
+
+wire addrRa;
+assign addrRa =V_SW[1:0];
+
+wire addrRb;
+assign addrRb=V_SW[3:2];
+
+wire addrW;
+assign addrW = V_SW[5:4];
+
+wire datW;
+assign datW = V_SW [9:6];
+
+//output displays
+
+wire [6:0] datOutRa;
+wire [6:0] datOutRb;
+
+
+// La cantdiad de registros es igual a: 
+localparam NREG = 2 ** BIT_ADDR;
+  
+//configiración del banco de registro 
+reg [BIT_DATO-1: 0] breg [NREG-1:0];
+
+
+assign  datOutRa = breg[addrRa];
+assign  datOutRb = breg[addrRb];
+
+always @(posedge clk) begin
+	if (RegWrite == 1)
+     breg[addrW] <= datW;
+  end
+
+
+
+//instancia de display
+BCDtoSSeg d2(.V_SW1(datOutRa), .G_HEX(G_HEX0));
+BCDtoSSeg d3(.V_SW1(datOutRb), .G_HEX(G_HEX1));
+
+endmodule
+
